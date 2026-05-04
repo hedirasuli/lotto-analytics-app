56d2c63a1e39b197697feb4923b7511ca00bd535
hedirasuli <hedi.rasuli21@gmail.com>
2026-05-04 20:45:14 +0200

    feat: initialize backend project structure and add core dependencies

---
+++ backend/main.py
@@ -0,0 +1,8 @@
+import random
+import pandas as pd
+import psycopg2
+import numpy as np
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from collections import Counter
+import warnings
