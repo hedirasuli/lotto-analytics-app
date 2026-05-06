b69817d9119098585079e6d505cdaf84755d905a
hedirasuli <hedi.rasuli21@gmail.com>
2026-05-06 18:40:15 +0200

    feat: uncomment imports for Tabs, Ionicons, and theme in layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,6 @@
-//import { Tabs } from 'expo-router';
-//import { Ionicons } from '@expo/vector-icons';
-//import { theme } from '../src/styles';
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { theme } from '../src/styles';
 
 export default function Layout() {
   return (
